1a8bf00398eba06b576e61204bed5ce5f1907ee4
SattiBabu77 <68766591+SattiBabu77@users.noreply.github.com>
2022-03-24 13:40:37 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Thank you Satish";
+        return "Thank you Karthik";
     }
 
 }
